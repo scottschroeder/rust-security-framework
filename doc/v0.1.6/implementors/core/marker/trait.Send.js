7f69e0de18dbc637fb6d0ecb5fcc525ee5f73692
Steven Fackler <sfackler@palantir.com>
2016-07-16 19:10:55 +0100

    Release v0.1.6

---
+++ doc/v0.1.6/implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["security_framework"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='security_framework/certificate/struct.SecCertificate.html' title='security_framework::certificate::SecCertificate'>SecCertificate</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='security_framework/identity/struct.SecIdentity.html' title='security_framework::identity::SecIdentity'>SecIdentity</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='security_framework/policy/struct.SecPolicy.html' title='security_framework::policy::SecPolicy'>SecPolicy</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='security_framework/secure_transport/struct.SslContext.html' title='security_framework::secure_transport::SslContext'>SslContext</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
