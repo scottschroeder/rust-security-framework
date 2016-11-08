2ebd301cee48dec4e110771381bcfaf8a5d3f057
Steven Fackler <sfackler@palantir.com>
2016-11-08 20:41:28 +0000

    Release v0.1.9

---
+++ doc/v0.1.9/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["security_framework"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;OSStatus&gt; for <a class='struct' href='security_framework/base/struct.Error.html' title='security_framework::base::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
