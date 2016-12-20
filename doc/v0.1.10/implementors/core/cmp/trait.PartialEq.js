89e8384078b25bd01ecef28edf77a53d108559a5
Steven Fackler <sfackler@gmail.com>
2016-12-19 19:38:20 -0800

    Release v0.1.10

---
+++ doc/v0.1.10/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["security_framework"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='security_framework/cipher_suite/enum.CipherSuite.html' title='security_framework::cipher_suite::CipherSuite'>CipherSuite</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='security_framework/secure_transport/enum.SslProtocol.html' title='security_framework::secure_transport::SslProtocol'>SslProtocol</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
