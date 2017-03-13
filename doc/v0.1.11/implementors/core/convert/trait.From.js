e3ea2464bac00d5d0d5f64890ddc85be06f7259d
Steven Fackler <sfackler@gmail.com>
2017-03-12 19:30:08 -0700

    Release v0.1.11

---
+++ doc/v0.1.11/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["security_framework"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;OSStatus&gt; for <a class='struct' href='security_framework/base/struct.Error.html' title='security_framework::base::Error'>Error</a>","impl&lt;S&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='security_framework/base/struct.Error.html' title='security_framework::base::Error'>Error</a>&gt; for <a class='enum' href='security_framework/secure_transport/enum.HandshakeError.html' title='security_framework::secure_transport::HandshakeError'>HandshakeError</a>&lt;S&gt;","impl&lt;S&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='security_framework/base/struct.Error.html' title='security_framework::base::Error'>Error</a>&gt; for <a class='enum' href='security_framework/secure_transport/enum.ClientHandshakeError.html' title='security_framework::secure_transport::ClientHandshakeError'>ClientHandshakeError</a>&lt;S&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
