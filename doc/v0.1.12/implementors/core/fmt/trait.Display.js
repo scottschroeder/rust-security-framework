96e02333ed5267b9dda029e930b232d5799eab20
Steven Fackler <sfackler@palantir.com>
2017-03-15 11:42:22 -0700

    Release v0.1.12

---
+++ doc/v0.1.12/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["security_framework"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='security_framework/base/struct.Error.html' title='security_framework::base::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
