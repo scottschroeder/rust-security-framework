f42647e7f81de40c263c6665877dab5149caaa98
Steven Fackler <sfackler@palantir.com>
2017-03-29 14:59:30 -0700

    Release v0.1.14

---
+++ doc/v0.1.14/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["security_framework"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='security_framework/base/struct.Error.html' title='security_framework::base::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
